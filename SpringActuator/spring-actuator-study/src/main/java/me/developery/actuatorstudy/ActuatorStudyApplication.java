3a56acb38e8226935d8f4a16cd492b673003e68f
김흥기 <jcscom13@gmail.com>
2025-03-08 22:19:28 +0900

    강의정리 Actuator(8장 작성중)

--- SpringActuator/spring-actuator-study/src/main/java/me/developery/actuatorstudy/ActuatorStudyApplication.java
+++ SpringActuator/spring-actuator-study/src/main/java/me/developery/actuatorstudy/ActuatorStudyApplication.java
@@ -2,8 +2,10 @@ package me.developery.actuatorstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "me.**")
 public class ActuatorStudyApplication {
 
 	public static void main(String[] args) {
