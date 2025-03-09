df70b4e94a20ecb200fca9b031adf2c566670fa6
김흥기 <jcscom13@gmail.com>
2025-03-09 21:20:29 +0900

    8. about metrics endpoint

--- SpringActuator/spring-actuator-study/src/main/java/me/developery/actuatorstudy/config/CountConfig.java
+++ SpringActuator/spring-actuator-study/src/main/java/me/developery/actuatorstudy/config/CountConfig.java
@@ -5,6 +5,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//CountedAspect 클래스를 bean 으로 등록
 @Configuration
 public class CountConfig {
 
